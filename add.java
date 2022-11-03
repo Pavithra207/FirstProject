6779652e1d9ae849d0b951a486e0e8ef1dd22d0f
Pavithra207 <117046974+Pavithra207@users.noreply.github.com>
2022-11-03 12:03:39 +0530

    updated hello worldy

--- add.java
+++ add.java
@@ -1,5 +1,5 @@
 class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello World!, happy day"); 
+        System.out.println("Hello Worldy!, happy day"); 
     }
 }
